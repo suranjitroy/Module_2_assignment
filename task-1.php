7a3a35194d1f9c32d3618f5ced726df1ae6b6b5a
suranjitroy <s.surunjit@gmail.com>
2023-09-19 15:45:56 +0600

    Assignment

---
+++ task-1.php
@@ -0,0 +1,58 @@
+<?php
+
+// For Loop
+
+function evenNumberForLoop($i,$count){
+
+    for($i; $i<=$count; $i+=2){
+            echo $i+1;
+            echo"<br>";  
+    }
+
+
+
+}
+
+evenNumberForLoop(1,20);
+
+
+
+// While Loop
+
+
+function evenNumberWhileLoop($i,$count){
+
+    $i;
+    while($i<$count){
+        echo $i+1;
+        $i+=2;
+        echo "<br>";
+    }
+
+
+
+}
+evenNumberWhileLoop(1,20);
+
+// Do While Loop
+
+function evenNumberDoWhileLoop($i,$count){
+
+    $i;
+    do{
+        echo $i+1;
+        $i+=2;
+        echo "<br>";
+    }
+    while($i<$count);
+
+
+
+}
+evenNumberDoWhileLoop(1,20);
+
+
+
+
+
+?>
---
+++ task-2.php
@@ -0,0 +1,15 @@
+<?php
+
+for($i=1; $i<=50; $i++){
+
+    if($i == 5){
+        continue;
+    }
+    echo $i;
+    echo"<br>";  
+}
+
+
+
+
+?>
